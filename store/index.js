b331dab83c96452a0c0cbf3f148551cd28529836
vilpilman <vilpilman@yahoo.com>
2020-02-16 00:52:47 +0200

    test

--- store/index.js
+++ store/index.js
@@ -23,12 +23,12 @@ export const actions = {
     });
     await commit('setBlogPosts', blogPosts);
 
-    let files2 = await require.context('~/assets/content/profile/', false, /\.json$/);
-    let profile = files2.keys().map(key => {
-      let res = files2(key);
-      res.slug = key.slice(2, -5);
-      return res;
-    });
-    await commit('setProfile', profile); 
+    // let files2 = await require.context('~/assets/content/profile/', false, /\.json$/);
+    // let profile = files2.keys().map(key => {
+    //   let res = files2(key);
+    //   res.slug = key.slice(2, -5);
+    //   return res;
+    // });
+    // await commit('setProfile', profile); 
   },
 };
