a2134d21a501a76f06f51891ef5c047a6377efd7
vilpilman <vilpilman@yahoo.com>
2020-02-16 04:09:41 +0200

    test

--- components/AboutSection.vue
+++ components/AboutSection.vue
@@ -17,7 +17,7 @@
 
                         <div v-for="item in $store.state.profile">
                           <span class="title-s">{{ item.name }}:</span> 
-                            {{ item.title }}
+                            {{ item.title }} {{ item.order }}
                           </div>
                         </div>
                       </div>
